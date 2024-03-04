f00b85548402b20c9e0bb8116e6fb7ca6c3ef71f
Mason Anderson <mta0627@utulsa.edu>
2024-03-04 14:44:10 -0600

    wrote code for problem 1

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -1,9 +1,33 @@
-def food_input():
-    user_input = input()
-    tokens = user_input.split()
-    #type you while  loop here
+'''
+Name: Mason Anderson
+Time: 3/4/2024 2:43 PM
 
+'''
+
+
+
+def food_input():
+    #type you while  loop here
     
+    string = ''
+    i=0
+    while string != 'quit':
+        
+        user_input = input()
+        tokens = user_input.split()
+
+        number = tokens[1]
+        string = tokens[0]
+
+        if string != 'quit':
+            if i == 0:
+                output = f'Eating {number} {string} a day keeps you happy and healthy.'
+            else:
+                output = output + f'\nEating {number} {string} a day keeps you happy and healthy.'
+
+        i += 1
+    
+    print (output)
 
 if __name__ == "__main__":
     food_input()

45b39350d507db3b8d0ca76cb2c98f81f415304a
Mason Anderson <mta0627@utulsa.edu>
2024-03-05 13:53:40 -0600

    wrote program for problem 2

--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,6 +1,17 @@
 def check_palindrome(user_input):
  #type your code here
-    pass
+    string = user_input.replace(' ','')
+    
+    reverse_string = ''
+    for char in iter(string):
+        reverse_string = char + reverse_string
+    
+    if string == reverse_string:
+        print (f'palindrome: {user_input}')
+    else:
+        print (f'not a palindrome: {user_input}')
+
+
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)

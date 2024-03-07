6b33152b48dd3b1009ea1fa4ac022a3d4a33bf17
Mason Anderson <mta0627@utulsa.edu>
2024-03-06 20:48:50 -0600

    Wrote code for problem 4

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -1,13 +1,32 @@
 def process_and_print(input_string):
       # Split into separate strings
 
+    input_data = input_string.split()
+
     # Convert strings to integers and filter out negative values
-    input_data = 0
+    
+    negative_numbers = []
+    for string in iter(input_data):
+        
+        num = int(string)
+
+        if num < 0:
+            negative_numbers.append(num)
 
     # Sort integers in reverse order
   
-    # Print sorted integers
+    negative_numbers.sort(reverse=True)
     
+    # Print sorted integers
+
+    output = ''
+    for num in iter(negative_numbers):
+        
+        string = str(num)
+
+        output += string + ' '
+    
+    print(output)
     
 
 if __name__ == "__main__":
